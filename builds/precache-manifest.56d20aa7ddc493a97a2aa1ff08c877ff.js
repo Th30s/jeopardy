e20d74fbdb45d93507e0857e08c9f58c47889167
Th30s <yahir.garcia@uasdc.org>
2020-06-18 21:14:35 +0000

    something

---
+++ builds/precache-manifest.56d20aa7ddc493a97a2aa1ff08c877ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ce5c767e364310df4166d41f24c59b",
+    "url": "/jeopardy_sol/index.html"
+  },
+  {
+    "revision": "21865fe5e0aa336d4e52",
+    "url": "/jeopardy_sol/static/css/main.82befaa4.chunk.css"
+  },
+  {
+    "revision": "6afeb4c32b5dfca7fbfb",
+    "url": "/jeopardy_sol/static/js/2.d10afcba.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/jeopardy_sol/static/js/2.d10afcba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21865fe5e0aa336d4e52",
+    "url": "/jeopardy_sol/static/js/main.d1ccac8d.chunk.js"
+  },
+  {
+    "revision": "045349e6b3875970389e",
+    "url": "/jeopardy_sol/static/js/runtime-main.9a9ee7f2.js"
+  }
+]);
